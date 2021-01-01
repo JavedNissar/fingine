95dc9aa86f102e30d4624b103213de3f1f481c8d
Javed Nissar <javednissar@protonmail.com>
2021-01-01 01:38:50 -0500

    Re-design tax engine
    Re-design tax engine to improve accuracy of calculations by relying
    on decimal.  Add multi-currency using
    rusty_money to enable the tax engine to work with
    USD later.

--- taxes/src/lib.rs
+++ taxes/src/lib.rs
@@ -1,69 +1,126 @@
+use std::{collections::HashMap};
+use rusty_money::{Money};
+use rust_decimal::prelude::*;
+
+macro_rules! decimal {
+    ($m:expr, $e:expr) => {
+       Decimal::new($m, $e) 
+    };
+}
 
 #[derive(Clone)]
 pub struct TaxBracket {
-    min: f64,
-    max: f64,
-    rate: f64,
+    min_money: Money,
+    max_money: Option<Money>,
+    rate: Decimal,
+}
+
+impl TaxBracket {
+    pub fn calculate_tax(&self, taxable_income: Decimal) -> Decimal{
+        if taxable_income < *self.min_money.amount() {
+            return Decimal::zero();
+        }
+
+        if let Some(actual_max_money) = &self.max_money {
+            if taxable_income >= *actual_max_money.amount() {
+                return actual_max_money.amount() * self.rate;
+            }else{
+                return (taxable_income - self.min_money.amount()) * self.rate;
+            }
+        }else{
+            return (taxable_income - self.min_money.amount()) * self.rate;
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct TaxDeduction{
+    tax_deduction_type: String,
+    max_amount: Money,
+    inclusion_rate: Decimal,
+}
+
+pub struct ActualTaxDeduction{
+    tax_deduction_type: String,
+    amount: Decimal,
+}
+
+pub enum TaxCalculationErrorCodes{
+    CouldNotFindDeduction,
 }
 
 #[derive(Clone)]
 pub struct TaxRegime {
     brackets: Vec<TaxBracket>,
+    deductions_map: HashMap<String, TaxDeduction>,
 }
 
 impl TaxRegime {
-    pub fn new(brackets: Vec<TaxBracket>) -> TaxRegime {
+    pub fn new(brackets: Vec<TaxBracket>, deductions_map: HashMap<String, TaxDeduction>) -> TaxRegime {
         let mut new_brackets = brackets.clone();
-        new_brackets.sort_by(|a,b| a.min.partial_cmp(&b.min).unwrap());
-        return TaxRegime { brackets: new_brackets }
-    }
-}
-
-fn calculate_tax_for_bracket(taxable_income: f64, tax_bracket: &TaxBracket) -> f64 {
-    if taxable_income < tax_bracket.min {
-        return 0.0
+        new_brackets.sort_by(|a,b| a.min_money.partial_cmp(&b.min_money).unwrap());
+        return TaxRegime { brackets: new_brackets, deductions_map: deductions_map}
     }
 
-    if taxable_income > tax_bracket.max {
-        return tax_bracket.max * tax_bracket.rate;
+    fn determine_deductions_amount(&self, deductions: Vec<ActualTaxDeduction>) -> Result<Decimal, TaxCalculationErrorCodes> {
+        deductions.iter().try_fold(decimal!(0, 0), |acc,actual_tax_deduction|{
+            match self.deductions_map.get(&actual_tax_deduction.tax_deduction_type){
+                Some(deduction_info) => {
+                    Ok(acc + actual_tax_deduction.amount * deduction_info.inclusion_rate)
+                },
+                None => Err(TaxCalculationErrorCodes::CouldNotFindDeduction)
+            }
+        })
     }
 
-    return (taxable_income - tax_bracket.min) * tax_bracket.rate;
-}
+    pub fn calculate_tax(&self, taxable_income: Decimal) -> Decimal{
+        self.brackets.iter().map(|bracket| bracket.calculate_tax(taxable_income)).sum()
+    }
 
-pub fn calculate_tax(taxable_income: f64, tax_regime: TaxRegime) -> f64 {
-    tax_regime.brackets.iter().map(|bracket| calculate_tax_for_bracket(taxable_income, bracket)).sum()
+    pub fn calculate_tax_with_deductions(&self, income: Decimal, deductions: Vec<ActualTaxDeduction>) -> Result<Decimal, TaxCalculationErrorCodes> {
+        let deductions_amount = self.determine_deductions_amount(deductions);
+        match deductions_amount {
+            Ok(deductions_total) => {
+                Ok(self.calculate_tax(income - deductions_total))
+            },
+            Err(error_code) => {
+                Err(error_code)
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use maplit::*;
     use super::*;
+    use rusty_money::money;
 
     #[test]
     fn simple_example() {
-        let lowest = TaxBracket { min: 0.0, max: 10000.0, rate: 0.1};
-        let middle = TaxBracket { min: 10000.0, max: 20000.0, rate: 0.2};
-        let highest = TaxBracket { min: 20000.0, max: f64::INFINITY, rate: 0.3};
+        let lowest = TaxBracket { min_money: money!(0.0, "CAD"), max_money: Some(money!(10000, "CAD")), rate: decimal!(01, 1)};
+        let middle = TaxBracket { min_money: money!(10000, "CAD"), max_money: Some(money!(20000,"CAD")), rate: decimal!(02, 1)};
+        let highest = TaxBracket { min_money: money!(20000, "CAD"), max_money: None, rate: decimal!(03, 1)};
 
-        let regime = TaxRegime::new(vec![lowest,middle,highest]);
+        let regime = TaxRegime::new(vec![lowest,middle,highest], hashmap!{});
 
-        let over_highest_tax = calculate_tax(25000.0, regime.clone());
-        assert_eq!(over_highest_tax, 6500.0);
+        let over_highest_tax = regime.calculate_tax(decimal!(25000, 0));
+        assert_eq!(over_highest_tax, decimal!(6500, 0));
 
-        let middle_tax = calculate_tax(15000.0, regime.clone());
-        assert_eq!(middle_tax, 2000.0);
+        let middle_tax = regime.calculate_tax(decimal!(15000, 0));
+        assert_eq!(middle_tax, decimal!(2000, 0));
 
-        let lowest_tax = calculate_tax(5000.0, regime);
-        assert_eq!(lowest_tax, 500.0);
+        let lowest_tax = regime.calculate_tax(decimal!(5000, 0));
+        assert_eq!(lowest_tax, decimal!(500, 0));
     }
 
     #[test]
     fn single_bracket_example(){
-        let lowest = TaxBracket { min: 0.0, max: 10000.0, rate: 0.1};
+        let lowest = TaxBracket { min_money: money!(0.0, "CAD"), max_money: Some(money!(10000.0, "CAD")), rate: decimal!(01,1)};
 
-        let regime = TaxRegime::new(vec![lowest]);
-        let tax = calculate_tax(10000.0, regime);
+        let regime = TaxRegime::new(vec![lowest], hashmap!{});
+        let tax = regime.calculate_tax(decimal!(10000,0));
 
-        assert_eq!(tax, 1000.0);
+        assert_eq!(tax, decimal!(1000,0));
     }
 }
