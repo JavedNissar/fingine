143aac9d695535ba9dd66aa9f797ecf1f383f799
Javed Nissar <javednissar@protonmail.com>
2021-01-10 13:25:45 -0500

    Updated rusty_money and improved utils to take ref

--- currency_utils/src/lib.rs
+++ currency_utils/src/lib.rs
@@ -1,5 +1,5 @@
 use anyhow::{Error, Result};
-use rusty_money::{Currency, Exchange, Money, MoneyError};
+use rusty_money::{FormattableCurrency, Exchange, Money, MoneyError};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -28,70 +28,70 @@ impl From<MoneyError> for ErrorCode {
 }
 
 
-fn convert(exchange: &Exchange, money: &Money, currency: &'static Currency) -> Result<Money, ErrorCode> {
+fn convert<'a, T: FormattableCurrency>(exchange: &'a Exchange<'a, T>, money: &Money<'a, T>, currency: &T) -> Result<Money<'a, T>, ErrorCode> {
     let exchange_rate_pair = exchange.get_rate(money.currency(), currency);
 
     if let Some(exchange_rate_pair) = exchange_rate_pair {
-        let cur_money = exchange_rate_pair.convert(*money)?;
+        let cur_money = exchange_rate_pair.convert(money.clone())?;
         Ok(cur_money)
     } else{
         Err(ErrorCode::CouldNotFindExchangeRate)
     }
 }
 
-pub trait CurrencyIndependentClamp {
-    fn clamp(&self, min_money: Money, max_money: Money, exchange: Exchange) -> Result<Money, ErrorCode>;
+pub trait CurrencyIndependentClamp<'a, T: FormattableCurrency> {
+    fn clamp(&self, min_money: &Money<'a, T>, max_money: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<Money<'a, T>, ErrorCode>;
 }
 
-pub trait CurrencyIndependentComparison {
-    fn currency_independent_lt(&self, other: Money, exchange: Exchange) -> Result<bool, ErrorCode>;
-    fn currency_independent_lte(&self, other: Money, exchange: Exchange) -> Result<bool, ErrorCode>;
-    fn currency_independent_gt(&self, other: Money, exchange: Exchange) -> Result<bool, ErrorCode>;
-    fn currency_independent_gte(&self, other: Money, exchange: Exchange) -> Result<bool, ErrorCode>;
-    fn currency_independent_eq(&self, other: Money, exchange: Exchange) -> Result<bool, ErrorCode>;
+pub trait CurrencyIndependentComparison<'a, T: FormattableCurrency> {
+    fn currency_independent_lt(&self, other: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<bool, ErrorCode>;
+    fn currency_independent_lte(&self, other: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<bool, ErrorCode>;
+    fn currency_independent_gt(&self, other: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<bool, ErrorCode>;
+    fn currency_independent_gte(&self, other: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<bool, ErrorCode>;
+    fn currency_independent_eq(&self, other: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<bool, ErrorCode>;
 }
 
-pub trait CurrencyIndependentAdd {
+pub trait CurrencyIndependentAdd<'a, T: FormattableCurrency> {
     fn add(
         &self,
-        other: Money,
-        output_currency: &'static Currency,
-        exchange: Exchange,
-    ) -> Result<Money, ErrorCode>;
+        other: &Money<'a, T>,
+        output_currency: &T,
+        exchange: &Exchange<'a, T>,
+    ) -> Result<Money<'a, T>, ErrorCode>;
 }
 
-pub trait CurrencyIndependentSub {
+pub trait CurrencyIndependentSub<'a, T: FormattableCurrency> {
     fn sub(
         &self,
-        other: Money,
-        output_currency: &'static Currency,
-        exchange: Exchange,
-    ) -> Result<Money, ErrorCode>;
+        other: &Money<'a, T>,
+        output_currency: &T,
+        exchange: &Exchange<'a, T>,
+    ) -> Result<Money<'a, T>, ErrorCode>;
 }
 
-impl CurrencyIndependentComparison for Money {
-    fn currency_independent_lt(&self, other: Money, exchange: Exchange) -> Result<bool, ErrorCode> {
-        let cur_money = convert(&exchange, self, other.currency())?;
+impl<'a, T: FormattableCurrency> CurrencyIndependentComparison<'a, T> for Money<'a, T> {
+    fn currency_independent_lt(&self, other: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<bool, ErrorCode> {
+        let cur_money = convert(exchange, self, other.currency())?;
         Ok(cur_money.amount() < other.amount())
     }
 
-    fn currency_independent_lte(&self, other: Money, exchange: Exchange) -> Result<bool, ErrorCode> {
-        let cur_money = convert(&exchange, self, other.currency())?;
+    fn currency_independent_lte(&self, other: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<bool, ErrorCode> {
+        let cur_money = convert(exchange, self, other.currency())?;
         Ok(cur_money.amount() <= other.amount())
     }
 
-    fn currency_independent_gt(&self, other: Money, exchange: Exchange) -> Result<bool, ErrorCode> {
-        let cur_money = convert(&exchange, self, other.currency())?;
+    fn currency_independent_gt(&self, other: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<bool, ErrorCode> {
+        let cur_money = convert(exchange, self, other.currency())?;
         Ok(cur_money.amount() > other.amount())
     }
 
-    fn currency_independent_gte(&self, other: Money, exchange: Exchange) -> Result<bool, ErrorCode> {
-        let cur_money = convert(&exchange, self, other.currency())?;
+    fn currency_independent_gte(&self, other: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<bool, ErrorCode> {
+        let cur_money = convert(exchange, self, other.currency())?;
         Ok(cur_money.amount() >= other.amount())
     }
 
-    fn currency_independent_eq(&self, other: Money, exchange: Exchange) -> Result<bool, ErrorCode> {
-        let cur_money = convert(&exchange, self, other.currency())?;
+    fn currency_independent_eq(&self, other: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<bool, ErrorCode> {
+        let cur_money = convert(exchange, self, other.currency())?;
         Ok(cur_money.amount() == other.amount())
     }
 }
@@ -102,10 +102,15 @@ pub enum PositionRelativeToRange{
     AfterRange,
 }
 
-fn determine_relative_position_of_money_relative_to_range(money_to_consider: Money, min_money: Money, max_money: Money, exchange: Exchange) -> Result<PositionRelativeToRange, ErrorCode> {
+fn determine_relative_position_of_money_relative_to_range<'a, T:FormattableCurrency>(
+    money_to_consider: &Money<'a, T>, 
+    min_money: &Money<'a, T>, 
+    max_money: &Money<'a, T>, 
+    exchange: &Exchange<'a, T>) 
+    -> Result<PositionRelativeToRange, ErrorCode> 
+{
     let less_than_min_result = money_to_consider.currency_independent_lt(min_money, exchange)?;
     let less_than_max_result = money_to_consider.currency_independent_lt(max_money, exchange)?;
-    let greater_than_or_equal_to_max_result = money_to_consider.currency_independent_gte(max_money, exchange)?;
 
     if less_than_min_result {
         Ok(PositionRelativeToRange::BeforeRange)
@@ -116,34 +121,30 @@ fn determine_relative_position_of_money_relative_to_range(money_to_consider: Mon
     }
 }
 
-impl CurrencyIndependentClamp for Money {
-    fn clamp(&self, min_money: Money, max_money: Money, exchange: Exchange) -> Result<Money, ErrorCode> {
-        let currency = self.currency();
-        let min_currency = min_money.currency();
-        let max_currency = max_money.currency();
-
-        let relative_to_range = determine_relative_position_of_money_relative_to_range(*self, min_money, max_money, exchange)?;
+impl<'a, T: FormattableCurrency> CurrencyIndependentClamp<'a, T> for Money<'a, T>{
+    fn clamp(&self, min_money: &Money<'a, T>, max_money: &Money<'a, T>, exchange: &Exchange<'a, T>) -> Result<Money<'a, T>, ErrorCode> {
+        let relative_to_range = determine_relative_position_of_money_relative_to_range(self, min_money, max_money, exchange)?;
         match relative_to_range {
-            PositionRelativeToRange::BeforeRange => convert(&exchange, &min_money, self.currency()),
-            PositionRelativeToRange::WithinRange => Ok(*self),
-            PositionRelativeToRange::AfterRange => convert(&exchange, &max_money, self.currency()),
+            PositionRelativeToRange::BeforeRange => convert(exchange, &min_money, self.currency()),
+            PositionRelativeToRange::WithinRange => Ok(self.clone()),
+            PositionRelativeToRange::AfterRange => convert(exchange, &max_money, self.currency()),
         }
     }
 }
 
-impl CurrencyIndependentAdd for Money {
-    fn add(&self, other: Money, output_currency: &'static Currency, exchange: Exchange) -> Result<Money, ErrorCode> {
-        let converted_self = convert(&exchange, &self, output_currency)?;
-        let converted_other = convert(&exchange, &other, output_currency)?;
+impl<'a, T: FormattableCurrency> CurrencyIndependentAdd<'a, T> for Money<'a, T>{
+    fn add(&self, other: &Money<'a, T>, output_currency: &T, exchange: &Exchange<'a, T>) -> Result<Money<'a, T>, ErrorCode> {
+        let converted_self = convert(exchange, &self, output_currency)?;
+        let converted_other = convert(exchange, &other, output_currency)?;
         
         Ok(converted_self + converted_other)
     }
 }
 
-impl CurrencyIndependentSub for Money {
-    fn sub(&self, other: Money, output_currency: &'static Currency, exchange: Exchange) -> Result<Money, ErrorCode> {
-        let converted_self = convert(&exchange, &self, output_currency)?;
-        let converted_other = convert(&exchange,&other, output_currency)?;
+impl<'a, T: FormattableCurrency> CurrencyIndependentSub<'a, T> for Money<'a, T>{
+    fn sub(&self, other: &Money<'a, T>, output_currency: &T, exchange: &Exchange<'a, T>) -> Result<Money<'a, T>, ErrorCode> {
+        let converted_self = convert(exchange, &self, output_currency)?;
+        let converted_other = convert(exchange,&other, output_currency)?;
 
         Ok(converted_self - converted_other)
     }
