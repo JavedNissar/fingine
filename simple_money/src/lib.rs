b2df00d26bbbb42bc0ac80572334d25f162192dd
Javed Nissar <javednissar@protonmail.com>
2021-02-09 07:59:34 -0500

    Implement stub ops for simple_money

--- simple_money/src/lib.rs
+++ simple_money/src/lib.rs
@@ -1,5 +1,8 @@
+use std::collections::HashMap;
+use std::ops::{Add, Sub};
 use rust_decimal::Decimal;
 
+#[derive(PartialEq, Eq, Hash)]
 enum Currency {
     CAD,
     USD,
@@ -8,4 +11,23 @@ enum Currency {
 struct Money {
     amount: Decimal,
     currency: Currency,
+    rates: HashMap<Currency,Decimal>,
+}
+
+impl Add for Money {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        // TODO: Handle different currencies
+        Self { amount: self.amount + other.amount, currency: self.currency, rates: self.rates}
+    }
+}
+
+impl Sub for Money {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        // TODO: Handle different currencies
+        Self { amount: self.amount - other.amount, currency: self.currency, rates: self.rates }
+    }
 }
