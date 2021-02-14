edae023c89ea394d5bb8bb0bc593ff5aab6c5674
Javed Nissar <javednissar@protonmail.com>
2021-02-14 13:09:44 -0500

    Implement tests and output currencies for some ops

--- simple_money/src/lib.rs
+++ simple_money/src/lib.rs
@@ -4,7 +4,7 @@ use std::cmp::Ordering;
 use rust_decimal::Decimal;
 use thiserror::Error;
 
-#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 enum Currency {
     CAD,
     USD,
@@ -18,10 +18,22 @@ pub enum MoneyError{
     MismatchedCurrencies,
 }
 
-#[derive(PartialEq, Eq, Clone, Copy)]
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
 struct Money {
     amount: Decimal,
     currency: Currency,
+}
+
+macro_rules! cad_money {
+    ($amount: expr) => {
+        Money { amount: dec!($amount), currency: Currency::CAD }
+    };
+}
+
+macro_rules! usd_money {
+    ($amount: expr) => {
+        Money { amount: dec!($amount), currency: Currency::USD }
+    };
 }
 
 #[derive(PartialEq, Eq, Hash)]
@@ -43,7 +55,7 @@ impl Exchange {
 
     pub fn set_rate(&mut self, from: Currency, to: Currency, rate: Decimal){
         let key = ExchangeRateQuery { from, to };
-        let inverse_key = ExchangeRateQuery{ to, from };
+        let inverse_key = ExchangeRateQuery{ from: to, to: from };
         self.rates.insert(key, rate);
         self.rates.insert(inverse_key, Decimal::new(1, 0) / rate);
     }
@@ -63,19 +75,23 @@ impl Exchange {
         return Ok(money * rate);
     }
     
-    pub fn add(&self, first: Money, second: Money) -> Result<Money, MoneyError> {
-        if first.currency == second.currency {
+    pub fn add(&self, first: Money, second: Money, output_currency: Currency) -> Result<Money, MoneyError> {
+        if first.currency == output_currency && second.currency == output_currency {
             Ok(first + second)
         }else{
-            self.convert(first, second.currency)
+            let first_in_output_currency = self.convert(first, output_currency)?;
+            let second_in_output_currency =self.convert(second, output_currency)?;
+            Ok(first_in_output_currency + second_in_output_currency)
         }
     }
 
-    pub fn sub(&self, first: Money, second: Money) -> Result<Money, MoneyError> {
-        if first.currency == second.currency {
+    pub fn sub(&self, first: Money, second: Money, output_currency: Currency) -> Result<Money, MoneyError> {
+        if first.currency == output_currency && second.currency == output_currency {
             Ok(first - second)
         }else{
-            self.convert(first, second.currency)
+            let first_in_output_currency = self.convert(first, output_currency)?;
+            let second_in_output_currency =self.convert(second, output_currency)?;
+            Ok(first_in_output_currency + second_in_output_currency)
         }
     }
 
@@ -115,14 +131,15 @@ impl Exchange {
         }
     }
 
-    pub fn clamp(&self, input: Money, min: Money, max: Money) -> Result<Money, MoneyError>{
-        if input.currency == min.currency && input.currency == max.currency {
+    pub fn clamp(&self, input: Money, min: Money, max: Money, output_currency: Currency) -> Result<Money, MoneyError>{
+        if input.currency == output_currency && min.currency == output_currency && max.currency == output_currency {
             Ok(input.clamp(min, max))
         }else{
-            let min_in_input_currency = self.convert(min, input.currency)?;
-            let max_in_input_currency = self.convert(max, input.currency)?;
+            let input_in_output_currency = self.convert(input, output_currency)?;
+            let min_in_output_currency = self.convert(min, output_currency)?;
+            let max_in_output_currency = self.convert(max, output_currency)?;
 
-            Ok(input.clamp(min_in_input_currency, max_in_input_currency))
+            Ok(input_in_output_currency.clamp(min_in_output_currency, max_in_output_currency))
         }
     }
 }
@@ -184,53 +201,201 @@ mod tests {
     use super::*;
     use rust_decimal_macros::*;
 
+    fn setup() -> Exchange {
+        let mut exchange = Exchange::new();
+        let rate = Decimal::new(13, 1);
+        exchange.set_rate(
+            Currency::USD, 
+            Currency::CAD, 
+            rate,
+        );
+        return exchange;
+    }
+
     #[test]
     fn can_compare_usd_amount_with_greater_cad_amount(){
+        let exchange = setup();
 
+        let usd_money = usd_money!(1);
+        let cad_money = cad_money!(2);
+
+        assert_eq!(exchange.lt(usd_money, cad_money).unwrap(), true);
+        assert_eq!(exchange.lte(usd_money, cad_money).unwrap(), true);
+        assert_eq!(usd_money == cad_money, false);
+        assert_eq!(exchange.gte(usd_money, cad_money).unwrap(), false);
+        assert_eq!(exchange.gt(usd_money, cad_money).unwrap(), false);
+
+        assert_eq!(exchange.lt(cad_money, usd_money).unwrap(), false);
+        assert_eq!(exchange.lte(cad_money, usd_money).unwrap(), false);
+        assert_eq!(cad_money == usd_money, false);
+        assert_eq!(exchange.gte(cad_money, usd_money).unwrap(), true);
+        assert_eq!(exchange.gt(cad_money, usd_money).unwrap(), true);
     }
 
     #[test]
     fn can_compare_usd_amount_with_less_cad_amount(){
+        let exchange = setup();
 
+        let usd_money = usd_money!(2);
+        let cad_money = cad_money!(1);
+
+        assert_eq!(exchange.lt(usd_money, cad_money).unwrap(), false);
+        assert_eq!(exchange.lte(usd_money, cad_money).unwrap(), false);
+        assert_eq!(usd_money == cad_money, false);
+        assert_eq!(exchange.gte(usd_money, cad_money).unwrap(), true);
+        assert_eq!(exchange.gt(usd_money, cad_money).unwrap(), true);
+
+        assert_eq!(exchange.lt(cad_money, usd_money).unwrap(), true);
+        assert_eq!(exchange.lte(cad_money, usd_money).unwrap(), true);
+        assert_eq!(cad_money == usd_money, false);
+        assert_eq!(exchange.gte(cad_money, cad_money).unwrap(), false);
+        assert_eq!(exchange.gt(cad_money, usd_money).unwrap(), false);
     }
 
     #[test]
     fn can_compare_usd_amount_with_equal_cad_amount(){
+        let exchange = setup();
 
+        let usd_money = usd_money!(1);
+        let cad_money = cad_money!(1.3);
+
+        assert_eq!(exchange.lt(usd_money, cad_money).unwrap(), false);
+        assert_eq!(exchange.lte(usd_money, cad_money).unwrap(), true);
+        assert_eq!(usd_money == cad_money, true);
+        assert_eq!(exchange.gte(usd_money, cad_money).unwrap(), true);
+        assert_eq!(exchange.gt(usd_money, cad_money).unwrap(), false);
+
+        assert_eq!(exchange.lt(cad_money, usd_money).unwrap(), false);
+        assert_eq!(exchange.lte(cad_money, usd_money).unwrap(), true);
+        assert_eq!(cad_money == usd_money, true);
+        assert_eq!(exchange.gte(cad_money, usd_money).unwrap(), true);
+        assert_eq!(exchange.gt(cad_money, usd_money).unwrap(), false);
     }
 
     #[test]
     fn can_clamp_with_value_less_than_range(){
+        let exchange = setup();        
 
+        let input = usd_money!(1);
+        let min = cad_money!(2);
+        let max = cad_money!(3);
+
+        let clamped_cad = exchange.clamp(input, min, max, Currency::CAD).unwrap();
+        let clamped_usd = exchange.clamp(input, min, max, Currency::USD).unwrap();
+
+        let expected_clamped_cad = cad_money!(2);
+        let expected_clamped_usd = usd_money!(1.57);
+
+        assert_eq!(clamped_cad, expected_clamped_cad);
+        assert_eq!(clamped_usd, expected_clamped_usd);
     }
 
     #[test]
     fn can_clamp_with_value_within_range(){
+        let exchange = setup();
 
+        let input = cad_money!(2.5);
+        let min = cad_money!(2);
+        let max = cad_money!(3);
+
+        let clamped_cad = exchange.clamp(input, min, max, Currency::CAD).unwrap();
+        let clamped_usd = exchange.clamp(input, min, max, Currency::USD).unwrap();
+
+        let expected_clamped_cad = cad_money!(2.5);
+        let expected_usd_amount = dec!(2.5) * (dec!(1)/dec!(1.3));
+        let expected_clamped_usd = Money { amount: expected_usd_amount, currency: Currency::USD };
+        
+        assert_eq!(clamped_cad, expected_clamped_cad);
+        assert_eq!(clamped_usd, expected_clamped_usd);
     }
 
     #[test]
     fn can_clamp_with_value_greater_than_range(){
+        let exchange = setup();
 
+        let input = cad_money!(2);
+        let min = usd_money!(0);
+        let max = usd_money!(0.5);
+
+        let clamped_cad = exchange.clamp(input, min, max, Currency::CAD).unwrap();
+        let clamped_usd = exchange.clamp(input, min, max, Currency::USD).unwrap();
+
+        let expected_clamped_cad = cad_money!(0.65);
+        let expected_clamped_usd = usd_money!(0.5);
+
+        assert_eq!(clamped_cad, expected_clamped_cad);
+        assert_eq!(clamped_usd, expected_clamped_usd);
     }
 
     #[test]
     fn can_add_different_currencies_and_get_converted_result(){
+        let exchange = setup();
 
+        let first = cad_money!(1); 
+        let second = usd_money!(1);
+        
+        let sum_in_cad = exchange.add(first, second, Currency::CAD).unwrap();
+        let sum_in_usd = exchange.add(first, second, Currency::USD).unwrap();
+
+        let expected_cad_sum = cad_money!(2.3);
+        let expected_usd_sum_amount = dec!(1) * dec!(1)/dec!(1.3) + dec!(1);
+        let expected_usd_sum = Money { amount: expected_usd_sum_amount, currency: Currency::USD };
+
+        assert_eq!(sum_in_cad, expected_cad_sum);
+        assert_eq!(sum_in_usd, expected_usd_sum);
     }
 
     #[test]
     fn can_add_same_currencies_and_get_converted_result(){
+        let exchange = setup();
 
+        let first = cad_money!(1);
+        let second = cad_money!(1);
+
+        let sum_in_cad = exchange.add(first, second, Currency::CAD).unwrap();
+        let sum_in_usd = exchange.add(first, second, Currency::USD).unwrap();
+
+        let expected_cad_sum = cad_money!(2);
+        let expected_usd_sum_amount = dec!(1) * dec!(1)/dec!(1.3) * dec!(2);
+        let expected_usd_sum = Money { amount: expected_usd_sum_amount, currency: Currency::USD };
+
+        assert_eq!(sum_in_cad, expected_cad_sum);
+        assert_eq!(sum_in_usd, expected_usd_sum);
     }
 
     #[test]
     fn can_subtract_different_currencies_and_get_converted_result(){
+        let exchange = setup();
 
+        let first = cad_money!(2);
+        let second = usd_money!(1);
+
+        let diff_in_cad = exchange.sub(first, second, Currency::CAD).unwrap();
+        let diff_in_usd = exchange.sub(first, second, Currency::USD).unwrap();
+
+        let expected_cad_diff = cad_money!(0.6);
+        let expected_usd_diff_amount = dec!(2) * dec!(1)/dec!(1.3) - dec!(1);
+        let expected_usd_diff = Money { amount: expected_usd_diff_amount, currency: Currency::USD };
+
+        assert_eq!(diff_in_cad, expected_cad_diff);
+        assert_eq!(diff_in_usd, expected_usd_diff);
     }
 
     #[test]
     fn can_subtract_same_currencies_and_get_converted_result(){
+        let exchange = setup();
 
+        let first = cad_money!(2);
+        let second = cad_money!(1);
+
+        let diff_in_cad = exchange.sub(first, second, Currency::CAD).unwrap();
+        let diff_in_usd = exchange.sub(first, second, Currency::USD).unwrap();
+
+        let expected_cad_diff = cad_money!(1);
+        let expected_usd_diff_amount = dec!(1) * dec!(1)/dec!(1.3);
+        let expected_usd_diff = Money { amount: expected_usd_diff_amount, currency: Currency::USD };
+
+        assert_eq!(diff_in_cad, expected_cad_diff);
+        assert_eq!(diff_in_usd, expected_usd_diff);
     }
 }
